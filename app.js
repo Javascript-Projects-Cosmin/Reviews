a523e07894bb5fbe770a78098fd6412d978ac7dc
onitiucosmin <48385722+onitiucosmin@users.noreply.github.com>
2021-04-04 16:49:00 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,97 @@
+// This is what we start with. Let's call it a simulation of a database of reviews. Each entry has 5 columns, the primary key, the id;
+//the name; the job; and image of the person; and the review itself. It is an array of objects
+const reviews = [
+    {
+      id: 1,
+      name: "susan smith",
+      job: "web developer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      text:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: "anna johnson",
+      job: "web designer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      text:
+        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    },
+    {
+      id: 3,
+      name: "peter jones",
+      job: "intern",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      text:
+        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    },
+    {
+      id: 4,
+      name: "bill anderson",
+      job: "the boss",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      text:
+        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+  ];
+  // Having our database of  reviews, let's think what is the scope of JS in this project. It is clearly to make it a dynamic page.
+  //What is dynamic in this page is the change of reviews, so let's grab in some variables what is needed to change the review
+  const img = document.getElementById("person-img");
+  const author = document.getElementById("author");
+  const job = document.getElementById("job");
+  const info = document.getElementById("info");
+
+  //Let's make some variables which grab the classes of the buttons we use on this simple page
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
+  const randomBtn = document.querySelector(".random-btn");
+  
+  // set starting item to nothing, so when the page starts, no review will be shown on the screen
+  let currentItem = 0;
+  
+  // Now, this is a new event, related to the window, so the screen that basically opens up
+  //What this does, is when the content is loaded does the next function which in a variable called
+  //item, stores the currentItem Object of reviews and changes the variables we talked above to 
+  //those of the currentItem Review.
+  window.addEventListener("DOMContentLoaded", function () {
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+  });
+  
+  // Now we will make a function similar to the event above, which we will use on the button presses
+  function showPerson(person) {
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+  }
+  // show next person
+  nextBtn.addEventListener("click", function () {
+    currentItem++;
+    //this is to make sure the clicking of buttons can go in a circle, and not stop dead
+    if (currentItem > reviews.length - 1) {
+      currentItem = 0;
+    }
+    showPerson(currentItem);
+  });
+  // show prev person
+  prevBtn.addEventListener("click", function () {
+    currentItem--;
+    if (currentItem < 0) {
+      currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+  });
+  // show random person
+  randomBtn.addEventListener("click", function () {
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson(currentItem);
+  });
